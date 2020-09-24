be0fed411d58d51e5f8f0284c8983b6f253ed7f5
Fernanda de Souza Caron <fernandadesouzacaron@gmail.com>
2020-09-24 20:04:06 -0300

    24set2020

--- simRangeOverSize.R
+++ simRangeOverSize.R
@@ -4,24 +4,35 @@ setwd("C:/Users/ferna/Documents/IC/overnets")
 
 source("codes/functions.R")
 
-rho<-seq(from = 0.01, to = 1, by = 0.01)
+rho<-seq(from = 0.001, to = 1, by = 0.002)
 meanlog<-rho #rho = meanlog/D(=1)
 S<-1000
 
-final1<-final2<-as.data.frame(matrix(ncol = 3, nrow = length(rho)))
-colnames(final1)<-colnames(final2)<-c("rho", "mean.range.size", "mean.range.overlap")
+final1<-final2<-as.data.frame(matrix(ncol = 9, nrow = length(rho)))
+colnames(final1)<-colnames(final2)<-c("rho", "mean.range.size", "mean.range.overlap",
+                                      "variance.range.size", "variance.range.overlap",
+                                      "skewness.range.size", "skewness.range.overlap",
+                                      "kurtosis.range.size", "kurtosis.range.overlap")
 
 for (i in 1:length(rho)){
   overlap<-simRangeOver(meanlog=meanlog[i], S=S, truncate=TRUE)
   
   #range size stats
   mean.range.size<-mean(diag(overlap))
+  variance.range.size<-var(diag(overlap))
+  skewness.range.size<-skewness(diag(overlap))
+  kurtosis.range.size<-kurtosis(diag(overlap))
   
   #range overlap stats
   diag(overlap)<-NA
-  mean.range.overlap <- mean(overlap, na.rm = TRUE)
-
-  res<-cbind(rho[i], mean.range.size, mean.range.overlap)
+  mean.range.overlap<-mean(overlap, na.rm = TRUE)
+  variance.range.overlap<-var(as.vector(overlap), na.rm = TRUE)
+  skewness.range.overlap<-skewness(as.vector(overlap), na.rm = TRUE)
+  kurtosis.range.overlap<-kurtosis(as.vector(overlap), na.rm = TRUE)
+  
+  res<-cbind(rho[i], mean.range.size, mean.range.overlap, variance.range.size, 
+             variance.range.overlap, skewness.range.size, skewness.range.overlap,
+             kurtosis.range.size, kurtosis.range.overlap)
   
   final1[i,]<-res
 }
@@ -31,30 +42,53 @@ for (i in 1:length(rho)){
   
   #range size stats
   mean.range.size<-mean(diag(overlap))
-
+  variance.range.size<-var(diag(overlap))
+  skewness.range.size<-skewness(diag(overlap))
+  kurtosis.range.size<-kurtosis(diag(overlap))
+  
   #range overlap stats
   diag(overlap) <- NA
   mean.range.overlap <- mean(overlap, na.rm = TRUE)
-
-  res<-cbind(rho[i], mean.range.size, mean.range.overlap)
+  variance.range.overlap<-var(as.vector(overlap), na.rm = TRUE)
+  skewness.range.overlap<-skewness(as.vector(overlap), na.rm = TRUE)
+  kurtosis.range.overlap<-kurtosis(as.vector(overlap), na.rm = TRUE)
+  
+  res<-cbind(rho[i], mean.range.size, mean.range.overlap, variance.range.size, 
+             variance.range.overlap, skewness.range.size, skewness.range.overlap,
+             kurtosis.range.size, kurtosis.range.overlap)
   
   final2[i,]<-res
 }
 
-layout(matrix(1:2, ncol=2))
+#Mean and Variance
+layout(matrix(1:4, ncol=2))
 
-plot(mean.range.overlap~mean.range.size, data=final1, ylim=c(0.3,0.8),  xlab="Range size", main="Truncated", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
+plot(mean.range.overlap~mean.range.size, data=final1, xlab="Range size", main="Mean", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
 abline(lm(mean.range.overlap~mean.range.size, data=final1))
-abline(a=0, b=1, col="red")
 
-plot(mean.range.overlap~mean.range.size, data=final2, xlim=c(1,5), xlab="Range size", main="", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
+plot(mean.range.overlap~mean.range.size, data=final2, xlab="Range size", main="", ylab = "Range overlap size", col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
 abline(lm(mean.range.overlap~mean.range.size, data=final2))
-abline(a=0, b=1, col="red")
 
-plot(mean.range.size~rho, data=final1, xlab = expression(rho), main="Range size", ylab = "Mean", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
-points(mean.range.size~rho, data=final2, col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
+plot(variance.range.overlap~variance.range.size, data=final1, xlab="Range size", main="Variance", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
+abline(lm(variance.range.overlap~variance.range.size, data=final1))
 
-plot(mean.range.overlap~rho, data=final1, xlab = expression(rho), main="Range overlap size", ylab = "Mean", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
-points(mean.range.overlap~rho, data=final2, col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
+plot(variance.range.overlap~variance.range.size, data=final2, xlab="Range size", main="", ylab = "Range overlap size", col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
+abline(lm(variance.range.overlap~variance.range.size, data=final2))
+
+
+#Skewness and Kurtosis 
+layout(matrix(1:4, ncol=2))
+
+plot(skewness.range.overlap~skewness.range.size, data=final1, xlab="Range size", main="Skewness", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
+abline(lm(skewness.range.overlap~skewness.range.size, data=final1))
+
+plot(skewness.range.overlap~skewness.range.size, data=final2, xlab="Range size", main="", ylab = "Range overlap size", col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
+abline(lm(skewness.range.overlap~skewness.range.size, data=final2))
+
+plot(kurtosis.range.overlap~kurtosis.range.size, data=final1, xlab="Range size", main="Kurtosis", ylab = "Range overlap size", col = rgb(0, 0, 139, 120, maxColorValue = 255), pch = 16)
+abline(lm(kurtosis.range.overlap~kurtosis.range.size, data=final1))
+
+plot(kurtosis.range.overlap~kurtosis.range.size, data=final2, xlab="Range size", main="", ylab = "Range overlap size", col = rgb(239, 106, 80, 120, maxColorValue = 255), pch = 16)
+abline(lm(kurtosis.range.overlap~kurtosis.range.size, data=final2))
 
 
